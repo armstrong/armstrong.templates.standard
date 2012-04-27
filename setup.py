5b18471232697273885078e1490d113afc0fd327
Travis Swicegood <development@domain51.com>
2012-04-27 16:51:38 -0500

    Add in missing fields

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from distutils.core import setup
 setup(
     name="armstrong.templates.standard",
     version="0.1alpha.0",
+    description="Provides a basic project template for an Armstrong project",
+    long_description=open("README.rst").read(),
+    author='Texas Tribune & Bay Citizen',
+    author_email='dev@armstrongcms.org',
     packages=[
         "armstrong.templates.standard",
     ],
