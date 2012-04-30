cc6e1f096a63c9f52dbee6779c143b6df1f11c05
Travis Swicegood <development@domain51.com>
2012-04-30 10:51:11 -0500

    Add missing package_data to file (v1.0.1)

--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
 from distutils.core import setup
+import os
+
+package_data = []
+BASE_DIR = os.path.dirname(__file__)
+walk_generator = os.walk(os.path.join(BASE_DIR, "project_template"))
+paths_and_files = [(paths, files) for paths, dirs, files in walk_generator]
+for path, files in paths_and_files:
+    prefix = path[len("project_template/"):]
+    if files:
+        package_data.append(os.path.join(prefix, "*.*"))
 
 setup(
     name="armstrong.templates.standard",
-    version="1.0.0",
+    version="1.0.1",
     description="Provides a basic project template for an Armstrong project",
     long_description=open("README.rst").read(),
     author='Texas Tribune & Bay Citizen',
@@ -12,6 +22,9 @@ setup(
     ],
     package_dir={
         "armstrong.templates.standard": "project_template",
+    },
+    package_data={
+        "armstrong.templates.standard": package_data,
     },
     namespace_packages=[
         "armstrong",
